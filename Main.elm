921b782f037271b37104c3be17f2227ea27af13f
Adam Legg <adam.legg@gmail.com>
2016-11-19 10:48:21 -0700

    added input

--- Main.elm
+++ Main.elm
@@ -1,22 +1,25 @@
-module Main exposing (..)
-
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
+import String
 
 
 -- model
 
 
 type alias Model =
-    Int
+    { calories : Int
+    , input : Int
+    , error : Maybe String
+    }
 
 
 initModel : Model
 initModel =
-    0
-
+    { calories = 0
+    , input = 0
+    , error = Nothing
+    }
 
 
 -- update
@@ -24,6 +27,7 @@ initModel =
 
 type Msg
     = AddCalorie
+    | Input String
     | Clear
 
 
@@ -31,10 +35,27 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddCalorie ->
-            model + 1
+            { model
+                | calories = model.calories + model.input
+                , input = 0
+            }
 
+        Input val ->
+            case String.toInt val of
+                Ok input ->
+                    { model
+                        | input = input
+                        , error = Nothing
+                    }
+
+                Err err ->
+                    { model
+                        | input = 0
+                        , error = Just err
+                    }
         Clear ->
             initModel
+
 
 
 
@@ -44,8 +65,19 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h3 []
-            [ text ("Total Calories: " ++ (toString model)) ]
+        [ h3 [] [ text ("Total Calories: " ++ (toString model.calories)) ]
+        , input
+            [ type_ "text"
+            , onInput Input
+            , value
+                (if model.input == 0 then
+                    ""
+                 else
+                    toString model.input
+                )
+            ]
+            []
+        , div [] [ text (Maybe.withDefault "" model.error) ]
         , button
             [ type_ "button"
             , onClick AddCalorie
@@ -56,7 +88,11 @@ view model =
             , onClick Clear
             ]
             [ text "Clear" ]
+        , p [] [ text (toString model) ]
         ]
+
+
+
 
 
 main =
