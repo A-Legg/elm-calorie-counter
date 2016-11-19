d791aef115144237734b12fd3b25e9de3ea14f37
Adam Legg <adam.legg@gmail.com>
2016-11-18 18:05:40 -0700

    initial commit

---
+++ Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+
+-- model
+
+
+type alias Model =
+    Int
+
+
+initModel : Model
+initModel =
+    0
+
+
+
+-- update
+
+
+type Msg
+    = AddCalorie
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            model + 1
+
+        Clear ->
+            initModel
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 []
+            [ text ("Total Calories: " ++ (toString model)) ]
+        , button
+            [ type_ "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type_ "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        ]
+
+
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
